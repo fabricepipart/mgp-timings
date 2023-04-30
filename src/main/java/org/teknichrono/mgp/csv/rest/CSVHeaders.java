3c48da8b8f7c2c4d565a4bfd54f2e0cfaf4bb3fc
Fabrice Pipart <fabrice.pipart@gmail.com>
2023-04-30 17:51:46 +0200

    #patch Improve Sonar metrics

--- src/main/java/org/teknichrono/mgp/csv/rest/CSVHeaders.java
+++ src/main/java/org/teknichrono/mgp/csv/rest/CSVHeaders.java
@@ -2,6 +2,4 @@ package org.teknichrono.mgp.csv.rest;
 
 public interface CSVHeaders {
 
-  String CONTENT_DISPOSITION_HEADER = "Content-Disposition";
-  String ATTACHMENT_FILENAME = "attachment;filename=";
 }
